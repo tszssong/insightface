e04c5149b7f093b06c49e1fdf31ece5b8ff97692
zhengmeisong <zhengmeisong@olgpu9.ai.shbt.qihoo.net>
2019-04-22 20:48:02 +0800

    update

--- recognition/train_m1.sh
+++ recognition/train_m1.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train.py --network m1 --loss softmax --dataset emore 2>&1 | tee ../../logs/m1.log
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train.py --network m1 --loss softmax --dataset emore 2>&1 | tee ../../logs/m1'%m_%d_%H_%M'.log
