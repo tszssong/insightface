56af02fa5acd3d4b8ae800aac7e59431742a2e6e
tszssong <zhengmeisong@126.com>
2019-05-07 17:55:16 +0800

    calculate face angle

---
+++ getRecImg/genLists/calAngle.py
@@ -0,0 +1,32 @@
+import sys, os
+import numpy as np
+listpath = sys.argv[1]
+widefile = open('./widelist.txt', 'w')
+widedir = open('./widedirlist.txt', 'w')
+print(listpath)
+imglist = open(listpath, 'r')
+yaw_dict = {}
+wide_file_list = []
+count = 0
+wide_cnt = 0
+for line in imglist.readlines():
+    items = line.strip().split('\t')
+    yaw = abs(int(float(items[4]))) #down to int
+    if not yaw in yaw_dict:
+        yaw_dict[yaw] = []
+    yaw_dict[yaw].append(items[1])
+    if yaw > 40:
+        wide_cnt += 1
+        widefile.write(line)
+        wide_path = items[1].split('/')[-2]
+        if not wide_path in wide_file_list:
+            wide_file_list.append(wide_path)
+    count += 1
+    if count % 1000 == 0:
+        print count, "processed!"
+print len(wide_file_list), "ids", wide_cnt, "faces has wide angle"
+for label in wide_file_list:
+    widedir.write(label+'\n')
+widedir.close()
+imglist.close()
+widefile.close()
