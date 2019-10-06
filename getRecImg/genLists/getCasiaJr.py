3320fb127db78b11f1417735925d718a0c548f28
zhengmeisong <zhengmeisong@test04.pm.corp.qihoo.net>
2019-10-06 21:12:29 +0800

    v100

---
+++ getRecImg/genLists/getCasiaJr.py
@@ -0,0 +1,39 @@
+import glob
+import numpy
+
+import os
+SDPath =  '/data3/zhengmeisong/data/gl2ms1m_dl23W1f1_150WW1_img/'
+FDSets = ['imgs/','xxx']
+
+fid = open('casia_jr.lst','w')
+count=0
+
+imgSet = FDSets[0]
+GatRoots = glob.glob(SDPath+imgSet+'/*')
+for idx2,imgID in enumerate(GatRoots):
+    sub_path = imgID.split('/')[-1] 
+    int_sub = int(sub_path.split('_')[-1])  
+    if int_sub<63834274:
+        continue
+    if idx2%1000==0:
+        print(idx2)
+    imgs=glob.glob(imgID+'/*.jpg')
+    for imgName in imgs:
+        fid.write('1\t'+imgName+'\t%d\n'%count)
+    count=count+1
+
+SDPath =  '/data3/zhengmeisong/data/jrPairs/'
+FDSets = ['imgs/','xxx']
+
+imgSet = FDSets[0]
+GatRoots = glob.glob(SDPath+imgSet+'/*')
+for idx2,imgID in enumerate(GatRoots):
+    if idx2%1000==0:
+        print(idx2)
+    imgs=glob.glob(imgID+'/*.jpg')
+    for imgName in imgs:
+        fid.write('1\t'+imgName+'\t%d\n'%count)
+    count=count+1
+
+fid.close()
+
---
+++ getRecImg/genLists/getJaCasiaJr.py
@@ -0,0 +1,52 @@
+import glob
+import numpy
+
+import os
+
+fid = open('ja_jr_casia.lst','w')
+count=0
+
+SDPath =  '/data3/zhengmeisong/data/jaTrain/'
+FDSets = ['imgs/','xxx']
+imgSet = FDSets[0]
+GatRoots = glob.glob(SDPath+imgSet+'/*')
+for idx2,imgID in enumerate(GatRoots):
+    if idx2%1000==0:
+        print(idx2)
+    imgs=glob.glob(imgID+'/*.jpg')
+    for imgName in imgs:
+        fid.write('1\t'+imgName+'\t%d\n'%count)
+    count=count+1
+
+
+SDPath =  '/data3/zhengmeisong/data/jrPairs/'
+FDSets = ['imgs/','xxx']
+
+imgSet = FDSets[0]
+GatRoots = glob.glob(SDPath+imgSet+'/*')
+for idx2,imgID in enumerate(GatRoots):
+    if idx2%1000==0:
+        print(idx2)
+    imgs=glob.glob(imgID+'/*.jpg')
+    for imgName in imgs:
+        fid.write('1\t'+imgName+'\t%d\n'%count)
+    count=count+1
+
+SDPath =  '/data3/zhengmeisong/data/gl2ms1m_dl23W1f1_150WW1_img/'
+FDSets = ['imgs/','xxx']
+imgSet = FDSets[0]
+GatRoots = glob.glob(SDPath+imgSet+'/*')
+for idx2,imgID in enumerate(GatRoots):
+    sub_path = imgID.split('/')[-1] 
+    int_sub = int(sub_path.split('_')[-1])  
+    if int_sub<63834274:
+        continue
+    if idx2%1000==0:
+        print(idx2)
+    imgs=glob.glob(imgID+'/*.jpg')
+    for imgName in imgs:
+        fid.write('1\t'+imgName+'\t%d\n'%count)
+    count=count+1
+
+fid.close()
+
