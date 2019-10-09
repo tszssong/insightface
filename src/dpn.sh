19800207bf9f304e6d647a2a33cae3c48eb75715
zhengmeisong <zhengmeisong@hpcgpu39.ai.zzzc.qihoo.net>
2019-10-09 09:42:04 +0800

    hpc39 dpn

--- src/dpn.sh
+++ src/dpn.sh
@@ -1,12 +1,12 @@
-DATA_DIR=/cloud_data01/zhengmeisong/TrainData/glintv2_emore_ms1m/
-
-NETWORK=p68
+DATA_DIR=/data04/zhengmeisong/TrainData/glintv2_emore_ms1m/
+NETWORK=p92
 MODELDIR="../../mx-models"
 PREFIX="$MODELDIR/model"
-CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' python -u train_softmax.py --data-dir $DATA_DIR \
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train_softmax.py --data-dir $DATA_DIR \
  --network "$NETWORK" \
  --loss-type 4 \
  --margin-m 0.5 --lr 0.1 \
  --lr-step 80000,160000,240000,320000 \
- --prefix "$PREFIX" --per-batch-size 40 --color 1 \
+ --emb-size 512 \
+ --prefix "$PREFIX" --per-batch-size 64 --color 1 \
  2>&1 | tee ../../mx-log/$NETWORK_`date +'%m_%d-%H_%M_%S'`.log
