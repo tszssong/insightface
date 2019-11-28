ad5f904772a15e782ba3446b2de5f4e311c6860d
zhengmeisong <zhengmeisong@hpcgpu36.ai.zzzc.qihoo.net>
2019-11-28 13:11:33 +0800

    update sh

--- src/eval/jaRev.sh
+++ src/eval/jaRev.sh
@@ -1,12 +1,13 @@
-DATAROOT=/cloud_data01/zhengmeisong/TestData/JA-Test/
+DATAROOT=/cloud_data01/zhengmeisong/TestData/JA-Rev-Test/
 MODEL=../../../mx-models/r50_tri_11_25/
+MODEL=../../../mx-models/m2_tri_11_23/
 for idx in `seq -f '%g' 9 9`
 do
   echo $idx
   python getFeature.py --model $MODEL/model,$idx \
                        --imlist ${DATAROOT}/imgs.lst
   python test_acc_ja.py --ftname '.ft' --model ${MODEL}/model,$idx \
-     --imgRoot $DATAROOT --ftSize 128\
+     --imgRoot $DATAROOT --ftSize 512\
      --idListFile id.lst  --faceListFile face.lst \
      --saveFP 0
 done
