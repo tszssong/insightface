5101af83f5f951f375c82651ff2dff4b99f0c387
tszssong <zhengmeisong@126.com>
2020-09-03 19:16:36 +0800

    megaface result

--- Evaluation/Megaface/run.sh
+++ Evaluation/Megaface/run.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
 
-DEVKIT="../../../megaface-devkit/devkit/experiments"
-ALGO="r100ii" #ms1mv2
-# ROOT=$(dirname `which $0`)
-ROOT=/cloud_data01/zhengmeisong/hpc36/myInsightface/Evaluation/Megaface/
+DEVKIT="//home/ubuntu/zms/wkspace/FR/devkit/experiments"
+ALGO="r100_10_07" #ms1mv2
+ROOT=$(dirname `which $0`)
+ROOT=/home/ubuntu/zms/wkspace/FR/myInsightface/Evaluation/Megaface/
 echo $ROOT
-python -u gen_megaface.py --gpu 0 --algo "$ALGO" --model '../../../mx-preTrain/model-r50-am-lfw/model,0'
+#python -u gen_megaface.py --gpu 0 --algo "$ALGO" --model './mxnet/model-r100-ii/model,0'
+python -u gen_megaface.py --gpu 0 --algo "$ALGO" --model './mxnet/model_r100_10_07/model,15'
 python -u remove_noises.py --algo "$ALGO"
 
 cd "$DEVKIT"
-# LD_LIBRARY_PATH="/usr/local/lib64:$LD_LIBRARY_PATH" 
-python -u run_experiment.py "$ROOT/fea_out_clean/megaface" "$ROOT/fea_out_clean/facescrub" _"$ALGO".bin ../../mx_results/ -s 100 -p ../templatelists/facescrub_features_list.json
+LD_LIBRARY_PATH="/usr/local/lib64:$LD_LIBRARY_PATH" python -u run_experiment.py "$ROOT/feature_out_clean/megaface" "$ROOT/feature_out_clean/facescrub" _"$ALGO".bin ../../mx_results/ -s 1000000 -p ../templatelists/facescrub_features_list.json
 cd -
+
